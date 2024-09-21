987946c2afcde8d0c886c5b0eca3874c48039b2a
Igor898718 <igor.zotov.saratov@yandex.ru>
2024-09-21 12:43:48 +0400

    Window settings

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import PyQt6
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon
+import sys
+
+app = QApplication(sys.argv)
+mainwindow = QWidget()
+mainwindow.setWindowTitle("AUR store")
+mainwindow.setWindowIcon(QIcon("images/archicon.png"))
+mainwindow.setMinimumSize(500, 500)
+mainwindow.show()
+
+
+
+
+app.exec()
 
